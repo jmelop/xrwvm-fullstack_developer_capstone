a268eeda04a0f616042261cdd479900985c95823
Juan Melo <jmelopdev@gmail.com>
2024-11-04 11:47:59 +0000

    fix: some small changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', '127.0.0.1', 
+    'localhost', '127.0.0.1',
     'https://jmelopdev-8000.theiadockernext-1-labs-prod-' +
     'theiak8s-4-tor01.proxy.cognitiveclass.ai',]
 CSRF_TRUSTED_ORIGINS = [
