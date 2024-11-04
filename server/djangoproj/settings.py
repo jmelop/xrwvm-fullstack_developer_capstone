bb10a0260068f933cd7b627a0c24c96fe8bbf5e1
Juan Melo <jmelopdev@gmail.com>
2024-11-04 12:27:21 +0000

    fix: some small changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,13 +29,18 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', '127.0.0.1',
-    'https://jmelopdev-8000.theiadockernext-1-labs-prod-' +
-    'theiak8s-4-tor01.proxy.cognitiveclass.ai',]
+    'localhost',
+    '127.0.0.1',
+    'jmelopdev-8000.theiadockernext-1-labs-prod-',
+    'theiak8s-4-tor01.proxy.cognitiveclass.ai',
+]
+
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1', 'http://localhost',
-    'https://jmelopdev-8000.theiadockernext-1-labs-prod-' +
-    'theiak8s-4-tor01.proxy.cognitiveclass.ai',]
+    'http://127.0.0.1',
+    'http://localhost',
+    'https://jmelopdev-8000.theiadockernext-1-labs-prod-',
+    'theiak8s-4-tor01.proxy.cognitiveclass.ai',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
