3cf96fcb192a773c5e32628c39c2516f3cbd1c98
Juan Melo <jmelopdev@gmail.com>
2024-11-04 11:47:09 +0000

    fix: some small changes

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,5 +4,12 @@
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+
+# Create superuser if it doesn't exist
+if ! python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='root').exists())"; then
+    echo "Creating superuser."
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'root@example.com', 'root')" | python manage.py shell
+fi
+
 python manage.py collectstatic --noinput
 exec "$@"
